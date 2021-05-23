f5f9c69f93b33f9862ebbae7779df35ffed5b30a
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-05-23 18:43:04 +0300

    Add files via upload

---
+++ code/Appointment.java
@@ -0,0 +1,6 @@
+import java.time.*;
+public class Appointment extends Farmer_Agriculturist_Communication{
+	private boolean availability;
+	LocalDateTime meetingDateTime;
+	
+}
---
+++ code/Service_Request.java
@@ -0,0 +1,5 @@
+
+public class Service_Request extends Farmer_Agriculturist_Communication{
+	private String textFile = "no value";
+	private boolean answerd;
+}
