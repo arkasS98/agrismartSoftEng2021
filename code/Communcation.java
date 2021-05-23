7c3dd0a515b7d04dbdc5c8816881caaf63e9b94a
Makxysik <66374186+Makxysik@users.noreply.github.com>
2021-05-23 19:36:11 +0300

    Add files via upload

---
+++ code/Communcation.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.time*;
+
+class Communication
+{
+    Farmer farmer;
+    Date date;
+    Time time;
+    protected String topic= "no value";
+    
+    
+    
+}
