5f087630cc9e25f4118349330d610a631220aeaf
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-05-23 17:24:28 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,9 @@
+public class User {
+	protected String userEmail = "no value";
+	protected String userPassword = "no value";
+	protected String username = "no value";
+	protected String name = "no value";
+	protected String surname = "no value";
+	protected String address = "no value";
+	protected int phoneNum;
+}
