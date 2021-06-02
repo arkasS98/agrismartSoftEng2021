aea7cedc3dc9a4fb55a017d41460217e7ccb4b1f
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-06-02 17:28:34 +0300

    Update User.java

--- code/User.java
+++ code/User.java
@@ -1,4 +1,6 @@
 public class User {
+	//attributes
+	
 	protected String userEmail = "no value";
 	protected String userPassword = "no value";
 	protected String username = "no value";
@@ -6,4 +8,91 @@ public class User {
 	protected String surname = "no value";
 	protected String address = "no value";
 	protected int phoneNum;
+	
+	
+	
+	
+	//Constructor
+	
+	public User(String mail, String pass, String usname, String fname, String lname, String addr, int num) {
+		this.userEmail = mail;
+		this.userPassword = pass;
+		this.username = usname;
+		this.name = fname;
+		this.surname = lname;
+		this.address = addr;
+		this.phoneNum = num;
+	}
+	
+	
+	
+	//Methods
+	
+	//      ********Sets & Gets********
+	public void setEmail(String st) {
+		this.userEmail = st;
+	}
+	
+	public String getEmail() {
+		return this.userEmail;
+	}
+	
+	
+	public void setPassword(String st) {
+		this.userEmail = st;
+	}
+	
+	public String getPassword() {
+		return this.userPassword;
+	}
+	
+	
+	public void setUsername(String st) {
+		this.username = st;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	
+	public void setName(String st) {
+		this.name = st;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	
+	public void setSurname(String st) {
+		this.surname = st;
+	}
+	
+	public String getSurname() {
+		return this.surname;
+	}
+	
+	
+	public void setAddress(String st) {
+		this.address = st;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	
+	
+	public void setPhoneNum(int n) {
+		this.phoneNum = n;
+	}
+	
+	public int getPhoneNum() {
+		return this.phoneNum;
+	}
+	//      ********Sets & Gets********
+	
+	
+	
+
 }
