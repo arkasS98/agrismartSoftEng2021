c88816d9ec5416205e905057c57dfd9c0d222550
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-06-04 20:43:55 +0300

    Update Agriculturist.java
    Methods για τον γεωπόνο, και λογικά θα κάνω και κάποιες προσθήκες σε methods άλλων κλάσεων όπου χρειάζεται!

--- code/Agriculturist.java
+++ code/Agriculturist.java
@@ -1,6 +1,55 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.time.*;
 
-public class Agriculturist {
-	ArrayList<Product> listOfProducts;
-	ArrayList<Tool> listOfTools;
+public class Agriculturist extends User{
+	
+	//attributes
+	ArrayList<Product> listOfProducts;	
+	ArrayList<Appointment> listOfAppointments;
+	ArrayList<Service_Request> listOfRequests;
+	
+	//Constructor 
+	public Agriculturist(String mail, String pass, String usname, String fname, String lname, String addr, int num) {
+		super(mail, pass, usname, fname, lname, addr, num);
+	}
+	
+	
+	//Methods 
+	
+	public void createAnnouncement(String text, Danger dang, String topic) {
+		Scanner textScan = new Scanner(System.in);
+		text = textScan.nextLine();
+		Scanner topicScan = new Scanner(System.in);
+		topic = topicScan.nextLine();
+		// Tha uparxei etoimo Danger h tha ftiaxnoume tote???
+		Announcement announ = new Announcement(text, dang, topic);
+	}
+	
+
+	public int todaysAppointCount() {
+		int count = 0;
+		LocalDate today = LocalDate.now();
+		if (!this.listOfAppointments.isEmpty())/*elegxoume an o xristis exei appointment genika*/ {
+			for (int i = 0; i < this.listOfAppointments.size(); i++) {
+				if(this.listOfAppointments.get(i).getDate().compareTo(today) == 0)//elegxoume posa rantevou exei gia shmera 
+					count++;
+			}
+			return count;                                                 //an count == 0 XWRIS
+		}                                                                 //NA EMFANISTEI MHNYMA
+		else {                                                            //AFTO SHMAINEI OTI
+			System.out.println("The appointment's list is empty.");       //DEN YPARXOUN RANTEVOU
+			return count;                                                 //MONO GIA SHMERA
+		}
+	}
+	
+	public void showAppointments() {
+		if (this.todaysAppointCount() > 0) {
+			int num;
+			for (int i = 0; i < this.todaysAppointCount(); i++) {
+				num = i + 1;
+				System.out.println("Appointment No." + num + ": " + this.listOfAppointments.get(i).getFarmer().getName() + " " + this.listOfAppointments.get(i).getFarmer().getSurname() + " " + this.listOfAppointments.get(i).getFarmer().getAddress() + " " + this.listOfAppointments.get(i).getTopic());
+			}
+		}
+	}
 }
