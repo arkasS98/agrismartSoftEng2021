d77db3daed5a2600a9a0b4026f60390b3f2a8218
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-06-05 19:34:59 +0300

    Update Farmer.java

--- code/Farmer.java
+++ code/Farmer.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 
-
-class Farmer extends User
-	{
- 	ArrayList <Product> ListOfProducts;
-    ArrayList <Cultivation> ListOfCultivations;
-    
-    
-   
-  	}
+public class Farmer extends User {
+	
+	//attributes
+	ArrayList<Cultivation> listOfCultivations /*= new ArrayList<Cultivation>()*/;
+	ArrayList<Product> listOfProducts /*= new ArrayList<Product>()*/;
+	
+	
+	//Constructor
+	
+	public Farmer(String mail, String pass, String usname, String fname, String lname, String addr, int num) {
+		super(mail, pass, usname, fname, lname, addr, num);
+	}
+}
