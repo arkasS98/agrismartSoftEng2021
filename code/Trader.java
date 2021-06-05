0cd02b6534239f942af2704c58fda938388039b0
Ιάσων Κωστόπουλος <13319178+Poleftaiger@users.noreply.github.com>
2021-06-05 19:09:41 +0300

    Update Trader.java

--- code/Trader.java
+++ code/Trader.java
@@ -1,4 +1,11 @@
-public class Trader {
-	business ListOfBusinesses;
-	prduct ListOfProducts;
+import java.util.ArrayList;
+
+public class Trader extends User{
+    private ArrayList<Business> listOfBusiness;
+    private ArrayList<Product> listOfProducts;
+
+    public Trader(String mail, String pass, String usname, String fname, String lname, String addr, int num) {
+        super(mail, pass, usname, fname, lname, addr, num);
+    }
 }
+
