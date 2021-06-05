f5931b77f1f6348def378cde6e40d4e2ad54d5fd
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-06-05 19:38:11 +0300

    Update and rename Communication_Farmer_Trader (2).java to Communication_Farmer_Trader.java

---
+++ code/Communication_Farmer_Trader.java
@@ -0,0 +1,13 @@
+class Communication_Farmer_Trader extends Communication{
+
+    Trader trader;
+    
+    
+    public Communication_Farmer_Trader(Farmer farmer, String topic) {
+        super(farmer, topic);
+    }
+
+
+
+
+}
