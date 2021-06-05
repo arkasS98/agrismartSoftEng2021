d9b2ccedcbff19ef769cd81f129ce51153cd63f7
Ιάσων Κωστόπουλος <13319178+Poleftaiger@users.noreply.github.com>
2021-06-05 17:49:16 +0300

    Update Appointment.java

--- code/Appointment.java
+++ code/Appointment.java
@@ -1,6 +1,42 @@
-import java.time*;
-
-public class Appointment extends Farmer_Agirculturist_Communication {
-	boolean availability;
-	LocalDateTime meetingDateTime;
+import java.time.*;
+public class Appointment extends Communication_Farmer_Agri{
+	//attributes
+	private boolean availability;
+	private LocalDate meetingDate;
+	private LocalTime meetingTime;
+	
+	//Constructor
+	public Appointment(Farmer farmer, Agriculturist agriculturist, String topic, LocalDate meetingdate, LocalTime meetingtime) {
+		super(farmer, agriculturist, topic);
+		this.meetingDate = meetingdate;
+		this.meetingTime = meetingtime;
+		this.availability = true;
+	}
+	
+	//Methods
+	public LocalDate getMeetingDate() {
+		return this.meetingDate;
+	}
+	
+	public LocalTime getMeetingTime() {
+		return this.meetingTime;
+	}
+	
+	public void setDate(LocalDate date) {
+		this.meetingDate = date;
+	}
+	
+	public void setTime(LocalTime time) {
+		this.meetingTime = time;
+	}
+	
+	public boolean isAvailable() {
+		return this.availability;
+	}
+	
+	public void setAvailability(boolean a) {
+		this.availability = a;
+	}
+	
+	
 }
