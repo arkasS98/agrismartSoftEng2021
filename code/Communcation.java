3d43f46af24b7dcc4523e278ad22b30f437aeada
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-06-05 16:48:12 +0300

    Update Communcation.java

--- code/Communcation.java
+++ code/Communcation.java
@@ -1,13 +1,46 @@
-import java.util.ArrayList;
-import java.time*;
+import java.time.*;
 
-class Communication
-{
-    Farmer farmer;
-    Date date;
-    Time time;
-    protected String topic= "no value";
-    
-    
-    
+public class Communication {
+	
+	//attributes
+	protected Farmer farmer;
+	protected LocalDate date;
+	protected LocalTime time;
+	protected String topic = "no value";
+	
+	//Constructor
+	public Communication(Farmer farmer, String topic) {
+		this.farmer = farmer;
+		this.topic = topic;
+		LocalDate dObj = LocalDate.now();
+		LocalTime tObj = LocalTime.now();
+		this.date = dObj;
+		this.time = tObj;
+	}
+	
+	
+	
+	//Methods
+	
+	//    *****Sets&Gets*****    
+	public LocalDate getDate() {
+		return this.date;
+	}
+	
+	public LocalTime getTime() {
+		return this.time;
+	}
+	
+	public Farmer getFarmer() {
+		return this.farmer;
+	}
+	
+	public String getTopic() {
+		return topic;
+	}
+	
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	//    *****Sets&Gets*****    
 }
