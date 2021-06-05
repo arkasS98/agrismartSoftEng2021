d5a583a9a81222207a578d41fe3967fc39a6d608
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-06-05 16:52:51 +0300

    Update Service_Request.java

--- code/Service_Request.java
+++ code/Service_Request.java
@@ -1,5 +1,32 @@
 
-public class Service_Request extends Farmer_Agriculturist_Communication{
-	private String textFile = "no value";
+public class Service_Request extends Communication_Farmer_Agri{
+	
+	//attributes
+	private String textField = "no value";
 	private boolean answerd;
+	
+	//Constructor
+	public Service_Request(Farmer farmer, Agriculturist agriculturist, String text, String topic) {
+		super(farmer, agriculturist, topic);
+		this.answerd = false;
+		this.textField = text;
+	}
+	
+	
+	//Methods
+	public String getextFiled() {
+		return this.textField;
+	}
+	
+	public boolean isAnswerd() {
+		return this.answerd;
+	}
+	
+	public void setTextField(String txt) {
+		this.textField = txt;
+	}
+	
+	public void setAnwerd(boolean ans) {
+		this.answerd = ans;
+	}
 }
