f49e6204e01bfaffc020bee74f14b601f1c80497
Ιάσων Κωστόπουλος <13319178+Poleftaiger@users.noreply.github.com>
2021-06-05 17:49:49 +0300

    Update Service_Request.java

--- code/Service_Request.java
+++ code/Service_Request.java
@@ -1,4 +1,32 @@
-public class Service_Request extends Farmer_Agriculturist_Communication{
-	string textField;
-	boolean answered;
+
+public class Service_Request extends Communication_Farmer_Agri{
+	
+	//attributes
+	private String textField = "no value";
+	private boolean answerd;
+	
+	//Constructor
+	public Service_Request(Farmer farmer, Agriculturist agriculturist, String text, String topic) {
+		super(farmer, agriculturist, topic);
+		this.answerd = false;
+		this.textField = text;
+	}
+	
+	
+	//Methods
+	public String getextFiled() {
+		return this.textField;
+	}
+	
+	public boolean isAnswerd() {
+		return this.answerd;
+	}
+	
+	public void setTextField(String txt) {
+		this.textField = txt;
+	}
+	
+	public void setAnwerd(boolean ans) {
+		this.answerd = ans;
+	}
 }
