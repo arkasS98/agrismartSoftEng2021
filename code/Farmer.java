62b88a18ea8e59a55067edc4deb9ef047777c559
Makxysik <66374186+Makxysik@users.noreply.github.com>
2021-06-05 22:32:03 +0300

    Update Farmer.java

--- code/Farmer.java
+++ code/Farmer.java
@@ -9,7 +9,12 @@ public class Farmer extends User {
 	
 	//Constructor
 	
-	public Farmer(String mail, String pass, String usname, String fname, String lname, String addr, int num) {
+	public Farmer(String mail, String pass, String usname, String fname, String lname, String addr, int num, Product product, Cultivation cultivation) {
 		super(mail, pass, usname, fname, lname, addr, num);
+		this.ListOfProducts = new ArrayList<Product>;
+	    	this.ListOfProducts.add(product);
+	    	
+	    	this.ListOfCultivations = new ArrayList<Cultivation>;
+	    	this.ListOfCultivations.add(cultivation);
 	}
 }
