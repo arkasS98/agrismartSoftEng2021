c8ea91262ea547f0cd835375cf8c05dad6053679
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-06-06 19:27:35 +0300

    Update Cultivation.java

--- code/Cultivation.java
+++ code/Cultivation.java
@@ -1,7 +1,51 @@
-
 public class Cultivation {
-private double latitude;
-private double longtitude;
-private double area;
-private String type;
+  //attributes
+  private double latitude;
+  private double longtitude;
+  private double area;
+  private String type;
+  
+  //constructor
+	public Cultivation(double lat, double longt, double area, String type) {
+		this.latitude = lat;
+		this.longtitude = longt;
+		this.area = area;
+		this.type = type;
+	}
+	
+	//methods
+	
+	//    *****Sets&Gets*****
+	public double getLatitude() {
+		return this.latitude;
+	}
+	
+	public double getLongtitude() {
+		return this.longtitude;
+	}
+	
+	public double getArea() {
+		return this.area;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+	
+	public void setLatitude(double d) {
+		this.latitude = d;
+	}
+	
+	public void setLongtitude(double d) {
+		this.longtitude = d;
+	}
+	
+	public void setArea(double d) {
+		this.area = d;
+	}
+	
+	public void setType(String s) {
+		this.type = s;
+	}
+	//  *****Sets&Gets*****
 }
