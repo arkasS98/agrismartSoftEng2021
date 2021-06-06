d17b3ea5712a902ad271aa4a5f975a13bbe2d101
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-06-06 19:26:04 +0300

    Update Business.java

--- code/Business.java
+++ code/Business.java
@@ -1,8 +1,51 @@
-
-
 public class Business {
- private String name;
- private String  address;
- private int  phoneNum;
- private int afm;
+	 //attributes
+	 private String name;
+	 private String  address;
+	 private int  phoneNum;
+	 private int afm;
+	 
+	 //constructor
+	 public Business (String name, String address, int phone, int afm) {
+		 this.name = name;
+		 this.address = address;
+		 this.phoneNum = phone;
+		 this.afm = afm;
+	 }
+	 
+	 //methods
+	 
+	 //    *****Sets&Gets*****
+	 public String getBusinessName() {
+		 return this.name;
+	 }
+	 
+	 public String getBusinessAddress() {
+		 return this.address;
+	 }
+	 
+	 public int getBusinessPhone() {
+		 return this.phoneNum;
+	 }
+	 
+	 public int getBusinessAfm() {
+		 return this.afm;
+	 }
+	 
+	 public void setBusinessName(String s) {
+		 this.name = s;
+	 }
+	 
+	 public void setBusinessAddress(String s) {
+		 this.address = s;
+	 }
+
+	 public void setBusinessPhone(int i) {
+		 this.phoneNum = i;
+	 }
+
+	 public void setBusinessAfm(int i) {
+		 this.afm = i;
+	 }
+	 //    *****Sets&Gets*****
 }
