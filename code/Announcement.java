30c1bb7a2b66e2526db41439ef649b86f21acd64
ΒΗΣΣΑΡΙΩΝ ΚΑΛΥΒΙΩΤΗΣ <79657008+arkasS98@users.noreply.github.com>
2021-06-06 19:25:10 +0300

    Update Announcement.java

--- code/Announcement.java
+++ code/Announcement.java
@@ -9,6 +9,12 @@ public class Announcement {
 	
 	
 	//Constructor
+	
+	public Announcement(Sting txt, String topic) {
+		this.textField = txt;
+		this.topic = topic;
+	}
+	
 	public Announcement(String txt, Danger danger, String topic) {
 		this.textField = txt;
 		this.listOfDangers = new ArrayList<Danger>();
@@ -16,5 +22,38 @@ public class Announcement {
 		this.topic = topic;
 	}
 	 
-	 
+//Methods
+	
+	//    *****Sets&Gets*****   
+	public String getTextField() {
+		return this.textField;
+	}
+	
+	public String getTopic() {
+		return this.topic;
+	}
+	
+	public ArrayList<Danger> getDangers() {
+		return this.listOfDangers;
+	}	
+	
+	
+	
+	public void setTextField(String txt) {
+		this.textField = txt;
+	}
+	
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	//    *****Sets&Gets*****
+	
+	public void printDangers() {
+		if (!this.listOfDangers.isEmpty()) {
+			for (int i = 0; i < this.listOfDangers.size(); i++)
+				System.out.println(this.listOfDangers.get(i).getTypeOfDanger() + "\t" + this.listOfDangers.get(i).getLocationOfDanger() + "\t" + this.listOfDangers.get(i).getSeverityOfDanger());
+		}
+		else 
+			System.out.println("This announcement contains no danger!");
+	}
 }
